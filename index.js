fcdbb492a0922e0db9892f32109f2f98f0c40fc3
Ignacio Gonzalez Chemes <nachogonza123@gmail.com>
2022-05-19 17:15:59 -0300

    Agrego ruta

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 let puerto = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
 
 app.listen(puerto, () => {
     console.log(`Servidor prendido en puerto ${puerto}`);
